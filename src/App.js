5d0419c1559989905e4fdb3da164612890f34b70
Deepank Sharma <aaronhazard007@gmail.com>
2021-09-21 22:17:58 +0530

    Added navbar details

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css';
 function App() {
     return (
         <div className="app">
             <div className="navbar">
-
+                <Navbar />
             </div>
             <div className="main">
-
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/">
+                                <Homepage />
+                            </Route>
+                            <Route exact path="/exchanges">
+                                <Exchanges />
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <Cryptocurrencies />
+                            </Route>
+                            <Route exact path="/crypto/:coinId">
+                                <CryptoDetails />
+                            </Route>
+                            <Route exact path="/news">
+                                <News />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
             </div>
             <div className="footer">
 
